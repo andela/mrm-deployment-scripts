ecf851a38266053bfd7ec6a952ac714325a2a74e
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-08-18 05:49:00 +0300

    [ch 159428898] reserve templates

--- terraform/metadata-config.tf
+++
@@ -1,7 +0,0 @@
-// Setup all project-wide metadata.
-
-resource "google_compute_project_metadata" "mrm_api_project_metadata" {
-  metadata {
-    mrm_vault_server_IP = "${lookup(var.static_ips, "vault-server")}"
-  }
-}
