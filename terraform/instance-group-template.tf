fbc168a62c1bb09a9078111890fb1a40b0fa47a9
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-08-18 05:37:25 +0300

    [ch 159428898] set metadata to instance group template

--- terraform/instance-group-template.tf
+++ terraform/instance-group-template.tf
@@ -10,6 +10,14 @@ resource "google_compute_instance_template" "frontend-template" {
     source_image = "mrm-frontend-packer-image"
   }
 
+  metadata {
+    MRM_URL             = "${var.mrm_url}"
+    MRM_API_URL         = "${var.mrm_api_url}"
+    ANDELA_LOGIN_URL    = "${var.andela_login_url}"
+    ANDELA_API_URL      = "${var.andela_api_url}"
+    mrm_vault_server_IP = "${lookup(var.static_ips, "vault-server")}"
+  }
+
   network_interface {
     subnetwork = "${google_compute_subnetwork.private-fe-be.self_link}"
   }
@@ -35,6 +43,15 @@ resource "google_compute_instance_template" "backend-template" {
   disk {
     boot         = "true"
     source_image = "mrm-backend-packer-image"
+  }
+
+  metadata {
+    APP_SETTINGS        = "${var.app_settings}"
+    MRM_URL             = "${var.mrm_url}"
+    MRM_API_URL         = "${var.mrm_api_url}"
+    ANDELA_LOGIN_URL    = "${var.andela_login_url}"
+    ANDELA_API_URL      = "${var.andela_api_url}"
+    mrm_vault_server_IP = "${lookup(var.static_ips, "vault-server")}"
   }
 
   network_interface {
