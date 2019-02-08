02fe672f4b8ae4287a68d6a9050ce250a008025a
Philip Kwesiga <phil.kwesiga@gmail.com>
2019-02-08 17:28:55 +0300

    feat(kubernetes) create cluster
    - create new cluster for converge
    - create network for the cluster
    - create a sub network for the cluster
    
    [Delivers #163824996]

---
+++ gke/variables.tf
@@ -0,0 +1,27 @@
+variable "project" {
+    type        ="string"
+}
+
+variable "region" {
+    type        = "string"
+}
+
+variable "zone" {
+    type        = "string"
+}
+
+variable "environment" {
+    type        = "string"
+}
+
+variable "prefix" {
+    type        = "string"
+}
+
+variable "credentials" {
+    type        = "string"
+}
+
+variable "bucket" {
+    type        = "string"
+}
