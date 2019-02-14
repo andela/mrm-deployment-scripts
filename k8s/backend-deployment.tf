9a150763f0b50e0067143262698e8618050e248f
Philip Kwesiga <phil.kwesiga@gmail.com>
2019-02-14 12:36:56 +0300

    feat(backend) Deployments
    - create namespace
    - create backend deployment
    - create backend service
    - create backend secret
    - use cloudsql proxy
    
    [Delivers #163512827]

---
+++ k8s/backend-deployment.tf
@@ -0,0 +1,65 @@
+resource "kubernetes_deployment" "backend" {
+    metadata {
+        name        = "${var.backend_name}"
+        namespace   = "${kubernetes_namespace.namespace.id}"
+        labels {
+            app     = "${var.backend_name}"
+        }
+    }
+
+    spec {
+        replicas    = 3
+
+        selector {
+            match_labels {
+                app = "${var.backend_name}"
+            }
+        }
+
+        template {
+            metadata {
+                namespace       = "${kubernetes_namespace.namespace.id}"
+                labels {
+                    app         = "${var.backend_name}"
+                }
+            }
+
+            spec {
+                container {
+                    image               = "${var.backend_image}"
+                    name                = "${var.backend_name}"
+                    port {
+                        container_port  = "${var.backend_container_port}"
+                        name            = "http"
+                    }
+                    
+                    image_pull_policy 	= "Always"
+                    command             = ["/app/docker/prod/start_gunicorn.sh"]
+                }
+                container {
+                    image               = "gcr.io/cloudsql-docker/gce-proxy:1.13"
+                    name                = "cloudsql-proxy"
+                    command             = ["/cloud_sql_proxy",
+                                            "-instances=converge-postgres=tcp:5432",
+                                            "-credential_file=/secrets/cloudsql/credentials.json",
+                                            "-log_debug_stdout=true"]
+                    security_context {
+                        run_as_user                 = 2
+                        allow_privilege_escalation  = "false"
+                    }
+                    volume_mount {
+                        name            = "cloudsql-instance-credentials"
+                        mount_path      = "/secrets/cloudsql"
+                        read_only       = "true"
+                    }
+                }
+                volume {
+                    name                = "cloudsql-instance-credentials"
+                    secret {
+                        secret_name     = "cloudsql-instance-credentials"
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ k8s/variables.tf
@@ -0,0 +1,20 @@
+variable "backend_name" {
+    type        ="string"
+}
+
+variable "credentials_file" {
+    type        ="string"
+}
+
+variable "namespace" {
+    type        ="string"
+}
+
+variable "backend_image" {
+    type        ="string"
+}
+
+variable "backend_container_port" {
+    type        ="string"
+}
+
