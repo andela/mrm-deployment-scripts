658150600f4e4d6586215474782a1f89bef49405
Davitron <segunmatthews@outlook.com>
2019-02-21 09:54:59 +0100

    feat(ingress controller): Integrate nginx Ingress  with terraform
    - create template ingress.yml file
    - create null_resource to hold ingress controller
    - update variables in main.tf
    
    [Finishes #164133294]

--- main.tf
+++ main.tf
@@ -16,7 +16,12 @@ module "k8s" {
     backend_name            = ""
     backend_image           = ""
     backend_container_port  = ""
-    frontend_name            = ""
-    frontend_image           = ""
-    frontend_container_port  = ""
+    frontend_name           = ""
+    frontend_image          = ""
+    frontend_container_port = ""
+    ingress_template_file   = ""
+    regional_static_ip      = ""
+    frontend_domain_name    = ""
+    backend_domain_name     = ""
 }
+
