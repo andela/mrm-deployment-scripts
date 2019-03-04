6b8d777473eb0587991de9d062c38960df757180
Philip Kwesiga <phil.kwesiga@gmail.com>
2019-03-04 11:03:14 +0300

    feat(microservice): deployment reseources
    - create namespace for the microservice
    - add deployment resource for the push notifications microservice
    - add kubernetes service push notifications
    - add microservice host to ingress
    
    [Delivers #164360919]

---
+++ k8s/microservice-deployment.tf
@@ -0,0 +1,42 @@
+resource "kubernetes_deployment" "microservice" {
+    metadata {
+        name        = "${var.microservice_name}"
+        namespace   = "${kubernetes_namespace.namespace.id}"
+        labels {
+            app     = "${var.microservice_name}"
+        }
+    }
+
+    spec {
+        replicas    = 3
+
+        selector {
+            match_labels {
+                app = "${var.microservice_name}"
+            }
+        }
+
+        template {
+            metadata {
+                namespace       = "${kubernetes_namespace.namespace.id}"
+                labels {
+                    app         = "${var.microservice_name}"
+                }
+            }
+
+            spec {
+                container {
+                    image               = "${var.microservice_image}"
+                    name                = "${var.microservice_name}"
+                    port {
+                        container_port  = "${var.microservice_container_port}"
+                        name            = "http"
+                    }
+                    
+                    image_pull_policy 	= "Always"
+                    command             = ["/app/docker/prod/start_gunicorn.sh"]
+                }
+            }
+        }
+    }
+}
