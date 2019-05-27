7d884f0bf99fcd0fa4974b2867b1b4aabda9bba9
Philip Kwesiga <phil.kwesiga@gmail.com>
2019-05-27 18:10:24 +0300

    feat(redis) kubernetes cluster (#71)
    - create terraform remote state
    - add terraform google provider
    - create redis vpc
    - create kubernetes cluster
    - ignore variable.tf file
    - ignore terraform files
    
    [Delivers #166266600]

---
+++ redis/network.tf
@@ -0,0 +1,12 @@
+resource "google_compute_network" "converge_redis_network" {
+    name                    = "converge-${var.environment}-vpc"
+    auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "converge_redis_subnet" {
+    name                        = "converge-${var.environment}-subnet"
+    ip_cidr_range               = "192.168.0.0/16"
+    region                      = "${var.region}"
+    network                     = "${google_compute_network.converge_redis_network.self_link}"
+    private_ip_google_access    = "true"
+}
