622cfa0cd3fef6012ec7748140c8400b646f8f4c
marcus chukwuoma <marcus2cu@gmail.com>
2019-08-12 15:57:55 +0100

    DA-401: Downgrade cluster machine-type
    - Change cluster machine type from n1-standard-1 (default) to g1-small
    
    [Delivers #401]

--- gke/cluster.tf
+++ gke/cluster.tf
@@ -16,5 +16,16 @@ resource "google_container_cluster" "converge_cluster" {
             auto_upgrade        = true
         }
     }]
+    node_config {
+        machine_type = "g1-small"
+        oauth_scopes = [
+            "https://www.googleapis.com/auth/devstorage.read_only",
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring",
+            "https://www.googleapis.com/auth/service.management.readonly",
+            "https://www.googleapis.com/auth/servicecontrol",
+            "https://www.googleapis.com/auth/trace.append",
+            "https://www.googleapis.com/auth/compute"
+        ]
+    }
 }
-
