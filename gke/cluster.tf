1e10f76b15b2c822bce4c4e9e8ec98e26169a85c
DevOps Learning <36444470+learning-devops@users.noreply.github.com>
2019-08-16 00:02:46 +0100

    Revert "fix cluster destroy and recreate issue"

--- gke/cluster.tf
+++ gke/cluster.tf
@@ -1,24 +1,31 @@
 
 resource "google_container_cluster" "converge_cluster" {
-  name       = "converge-${var.environment}"
-  location   = "${var.zone}"
-  network    = "${google_compute_network.converge_network.self_link}"
-  subnetwork = "${google_compute_subnetwork.converge_subnet.self_link}"
+  name                     = "converge-${var.environment}"
+  location                 = "${var.zone}"
+  network                  = "${google_compute_network.converge_network.self_link}"
+  subnetwork               = "${google_compute_subnetwork.converge_subnet.self_link}"
+  remove_default_node_pool = true
+  initial_node_count       = 2
+}
 
+resource "google_container_node_pool" "converge_node_pool" {
+  name       = "default-pool"
+  location   = "${var.zone}"
+  cluster    = "${google_container_cluster.converge_cluster.name}"
   node_count = 2
+
   autoscaling {
     min_node_count = 2
     max_node_count = 5
   }
+
   management {
     auto_upgrade = true
   }
 
   node_config {
     machine_type = "g1-small"
-    metadata {
-      disable-legacy-endpoints = "true"
-    }
+
     oauth_scopes = [
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
