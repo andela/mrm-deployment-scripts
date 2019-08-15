9d01f2c672fd73fe59c5769c5a1107f4b8aa87c6
marcus chukwuoma <marcus2cu@gmail.com>
2019-08-16 00:24:35 +0100

    fix cluster destroy and recreate issue

--- gke/cluster.tf
+++ gke/cluster.tf
@@ -5,18 +5,18 @@ resource "google_container_cluster" "converge_cluster" {
   network                  = "${google_compute_network.converge_network.self_link}"
   subnetwork               = "${google_compute_subnetwork.converge_subnet.self_link}"
   remove_default_node_pool = true
-  initial_node_count       = 2
+  initial_node_count       = 1
 }
 
 resource "google_container_node_pool" "converge_node_pool" {
   name       = "default-pool"
   location   = "${var.zone}"
   cluster    = "${google_container_cluster.converge_cluster.name}"
-  node_count = 2
+  node_count = 3
 
   autoscaling {
-    min_node_count = 2
-    max_node_count = 5
+    min_node_count = 3
+    max_node_count = 6
   }
 
   management {
@@ -25,6 +25,9 @@ resource "google_container_node_pool" "converge_node_pool" {
 
   node_config {
     machine_type = "g1-small"
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/devstorage.read_only",
